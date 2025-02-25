80e6ed7889e8b7229cf29c0e8e03ac2fdb710d29
Anushka-Singh1 <anushka.sng2002@gmail.com>
2025-02-25 09:52:58 +0530

    Basic setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center bg-gray-900">
+        <h1 className="text-4xl font-bold text-white">
+          Shankh Technology! 🎉
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App
